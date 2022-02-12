42aea45eee58a94f6ed5107dd5e988cd547df9e7
Kenan Gagnon <kenan.gagnon@gmail.com>
2022-02-12 18:46:32 +0100

    Team

---
+++ pages/Team.js
@@ -0,0 +1,16 @@
+const Team = () => {
+    return (
+        <>
+            <h1>Denis Gagnon, Maxim Gagnon et Kenan Gagnon</h1>
+        </>
+    )
+}
+
+export const getStaticProps = (context) => {
+    return { 
+        props: {},
+        revalidate: 10 
+    }
+}
+
+export default Team;
